101d2dc66302785a9401e412a6cd86b2c5dde616
Mustafa Caglar <mc934@cam.ac.uk>
2018-04-26 10:15:10 +0100

    Encapsulated ECD function

--- coolwater_translocation.m
+++ coolwater_translocation.m
@@ -87,21 +87,6 @@ end
 
 [good_translocations] = coolwater_see_translocations(all_translocations);
 
-ecds = [];
-
-for i=1:length(good_translocations)
-    data = good_translocations(i);
-    ecds(i) = coolwater_integrate(data);
-end
-
-    function [area] = coolwater_integrate(data)
-        area = 0;
-        time = data(:,2);
-        current = data(:,1);
-        for x = 2:2:length(current)
-            step = time(x)-time(x-1);
-            area = area + (step*(current(x)-current(x-1)));
-        end
-    end
+[ecds] = coolwater_ECD(good_translocations);
 
 end
